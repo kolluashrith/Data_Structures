89a1cac10d6658a6a14ab5cee5826e186376e189
kolluashrith <akollu1@jh.edu>
2026-04-23 01:37:52 -0400

    Added a bunch of tests for GraphADT

--- hw_starters/hw7-starter/src/test/java/hw7/GraphTest.java
+++ hw_starters/hw7-starter/src/test/java/hw7/GraphTest.java
@@ -1,12 +1,13 @@
 package hw7;
 
+import exceptions.InsertionException;
 import exceptions.PositionException;
+import exceptions.RemovalException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public abstract class GraphTest {
 
@@ -23,7 +24,41 @@ public abstract class GraphTest {
   @DisplayName("insert(v) returns a vertex with given data")
   public void canGetVertexAfterInsert() {
     Vertex<String> v1 = graph.insert("v1");
-    assertEquals(v1.get(), "v1");
+    assertEquals("v1", v1.get());
+  }
+
+  @Test
+  @DisplayName("insert(v) throws exception when v is null")
+  public void insertNullVertexThrowsException() {
+    try {
+      Vertex<String> v1 = graph.insert(null);
+      fail("InsertionException should have been thrown for null vertex");
+    } catch (InsertionException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("insert(v) throws exception when v is already in graph")
+  public void insertDuplicateVertexThrowsException() {
+    try {
+      Vertex<String> v1 = graph.insert("v1");
+      Vertex<String> v1_duplicate = graph.insert("v1");
+      fail("InsertionException should have been thrown for duplicate vertex");
+    } catch (InsertionException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("insert(v) handles multiple vertices")
+  public void insertMultipleVertices() {
+    Vertex<String> v1 = graph.insert("v1");
+    Vertex<String> v2 = graph.insert("v2");
+    Vertex<String> v3 = graph.insert("v3");
+    assertEquals("v1", v1.get());
+    assertEquals("v2", v2.get());
+    assertEquals("v3", v3.get());
   }
 
   @Test
@@ -32,12 +67,12 @@ public abstract class GraphTest {
     Vertex<String> v1 = graph.insert("v1");
     Vertex<String> v2 = graph.insert("v2");
     Edge<String> e1 = graph.insert(v1, v2, "v1-v2");
-    assertEquals(e1.get(), "v1-v2");
+    assertEquals("v1-v2", e1.get());
   }
 
   @Test
   @DisplayName("insert(null, V, e) throws exception")
-  public void insertEdgeThrowsPositionExceptionWhenfirstVertexIsNull() {
+  public void insertEdgeThrowsPositionExceptionWhenFirstVertexIsNull() {
     try {
       Vertex<String> v = graph.insert("v");
       graph.insert(null, v, "e");
@@ -46,6 +81,240 @@ public abstract class GraphTest {
       return;
     }
   }
+
+  @Test
+  @DisplayName("insert(V, null, e) throws exception")
+  public void insertEdgeThrowsPositionExceptionWhenSecondVertexIsNull() {
+    try {
+      Vertex<String> v = graph.insert("v");
+      graph.insert(v, null, "e");
+      fail("The expected exception was not thrown");
+    } catch (PositionException ex) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("insert(V1, V2, null) doesn't throw exception")
+  public void insertEdgeDoesNotThrowPositionExceptionWhenEdgeLabelIsNull() {
+    try {
+      Vertex<String> v1 = graph.insert("v1");
+      Vertex<String> v2 = graph.insert("v2");
+      graph.insert(v1, v2, null);
+      return;
+    } catch (PositionException ex) {
+      fail("Exception was thrown for null edge when it should not have been");
+    }
+  }
+
+  @Test
+  @DisplayName("insert(V, V, e) throws InsertionException for self-loop")
+  public void insertSelfLoopThrowsInsertionException() {
+    try {
+      Vertex<String> v = graph.insert("v");
+      graph.insert(v, v, "e");
+      fail("InsertionException should have been thrown for self-loop");
+    } catch (InsertionException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("insert duplicate edge throws InsertionException")
+  public void insertDuplicateEdgeThrowsInsertionException() {
+    try {
+      Vertex<String> v1 = graph.insert("v1");
+      Vertex<String> v2 = graph.insert("v2");
+      graph.insert(v1, v2, "e1");
+      graph.insert(v1, v2, "e2");
+      fail("InsertionException should have been thrown for duplicate edge");
+    } catch (InsertionException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("inserting edge in opposite direction doesn't throw InsertionException")
+  public void insertOppositeDirectionEdgeDoesNotThrowInsertionException() {
+    Vertex<String> v1 = graph.insert("v1");
+    Vertex<String> v2 = graph.insert("v2");
+    graph.insert(v1, v2, "forward");
+    Edge<String> reverse = graph.insert(v2, v1, "backward");
+    assertEquals("backward", reverse.get());
+  }
+
+  @Test
+  @DisplayName("insert edge with vertex from another graph throws PositionException")
+  public void insertEdgeWithVertexFromDifferentGraphThrowsPositionException() {
+    Graph<String, String> graph2 = createGraph();
+    Vertex<String> vertexFrom2 = graph2.insert("v1");
+    Vertex<String> vertexFrom1 = graph.insert("v2");
+    try {
+      graph.insert(vertexFrom2, vertexFrom1, "e");
+      fail("PositionException should have been thrown for vertex not belonging to this graph");
+    } catch (PositionException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("remove(v) returns element of removed vertex")
+  public void removeVertexReturnsElement() {
+    Vertex<String> v1 = graph.insert("v1");
+    assertEquals("v1", graph.remove(v1));
+  }
+
+  @Test
+  @DisplayName("remove(v) fully removes vertex from graph")
+  public void removeVertexFullyRemovesIt() {
+    Vertex<String> v1 = graph.insert("v1");
+    graph.remove(v1);
+    Iterable<Vertex<String>> vertices = graph.vertices();
+    for (Vertex<String> v : vertices) {
+      assertNotEquals("v1", v.get());
+    }
+  }
+
+  @Test
+  @DisplayName("remove(v) throws RemovalException when vertex is source of one or more edges")
+  public void removeVertexWithRemainingEdgesThrowsRemovalException() {
+    try {
+      Vertex<String> v1 = graph.insert("v1");
+      Vertex<String> v2 = graph.insert("v2");
+      graph.insert(v1, v2, "e");
+      graph.remove(v1);
+      fail("RemovalException should have been thrown");
+    } catch (RemovalException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("remove(v) throws RemovalException when vertex is the endpoint of an edge")
+  public void removeVertexThatIsEdgeEndpointThrowsRemovalException() {
+    try {
+      Vertex<String> v1 = graph.insert("v1");
+      Vertex<String> v2 = graph.insert("v2");
+      graph.insert(v1, v2, "e");
+      graph.remove(v2);
+      fail("RemovalException should have been thrown");
+    } catch (RemovalException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("remove(v) throws PositionException for null vertex")
+  public void removeNullVertexThrowsPositionException() {
+    try {
+      graph.remove((Vertex<String>) null); //Need cast for ambiguous call
+      fail("PositionException should have been thrown");
+    } catch (PositionException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("remove(v) throws PositionException for vertex that was already removed")
+  public void removeVertexAgainThrowsPositionException() {
+    try {
+      Vertex<String> v1 = graph.insert("v1");
+      graph.remove(v1);
+      graph.remove(v1);
+      fail("PositionException should have been thrown for trying to remove a previously removed vertex");
+    } catch (PositionException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("remove(v) permits inserting same vertex again after removal")
+  public void canReinsertVertexAfterRemoval() {
+    Vertex<String> v1 = graph.insert("v1");
+    graph.remove(v1);
+    Vertex<String> v1_again = graph.insert("v1");
+    assertEquals("v1", v1_again.get());
+  }
+
+  @Test
+  @DisplayName("remove(e) returns element of removed edge")
+  public void removeEdgeReturnsElement() {
+    Vertex<String> v1 = graph.insert("v1");
+    Vertex<String> v2 = graph.insert("v2");
+    Edge<String> e = graph.insert(v1, v2, "v1-v2");
+    assertEquals("v1-v2", graph.remove(e));
+  }
+
+  @Test
+  @DisplayName("remove(e) fully removes edge from graph")
+  public void removeEdgeFullyRemovesIt() {
+    Vertex<String> v1 = graph.insert("v1");
+    Vertex<String> v2 = graph.insert("v2");
+    Edge<String> e = graph.insert(v1, v2, "v1-v2");
+    graph.remove(e);
+    Iterable<Edge<String>> edges = graph.edges();
+    for (Edge<String> edge : edges) {
+      assertNotEquals("v1-v2", edge.get());
+    }
+  }
+
+  @Test
+  @DisplayName("remove(e) removes edges to allow vertices to be removed")
+  public void removeEdgeFreesVertices() {
+    Vertex<String> v1 = graph.insert("v1");
+    Vertex<String> v2 = graph.insert("v2");
+    Edge<String> e1 = graph.insert(v1, v2, "e1");
+    Edge<String> e2 = graph.insert(v2, v1, "e2");
+    graph.remove(e1);
+    graph.remove(e2);
+    graph.remove(v1);
+    graph.remove(v2);
+
+    Iterable<Vertex<String>> vertices = graph.vertices();
+    for (Vertex<String> v : vertices) {
+      assertNotEquals("v1", v.get());
+      assertNotEquals("v2", v.get());
+    }
+  }
+
+  @Test
+  @DisplayName("remove(e) throws PositionException for null edge")
+  public void removeNullEdgeThrowsPositionException() {
+    try {
+      graph.remove((Edge<String>) null); //Need cast to avoid ambiguous call
+      fail("PositionException should have been thrown");
+    } catch (PositionException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("remove(e) throws PositionException for previously removed edge")
+  public void removePreviouslyRemovedEdgeThrowsPositionException() {
+    try {
+      Vertex<String> v1 = graph.insert("v1");
+      Vertex<String> v2 = graph.insert("v2");
+      Edge<String> e = graph.insert(v1, v2, "e");
+      graph.remove(e);
+      graph.remove(e);
+      fail("PositionException should have been thrown for removing same edge again");
+    } catch (PositionException e) {
+      return;
+    }
+  }
+
+  @Test
+  @DisplayName("remove(e) allows edge to be inserted again after its removal")
+  public void canReinsertEdgeAfterRemoval() {
+    Vertex<String> v1 = graph.insert("v1");
+    Vertex<String> v2 = graph.insert("v2");
+    Edge<String> e = graph.insert(v1, v2, "e");
+    graph.remove(e);
+    Edge<String> e_again = graph.insert(v1, v2, "e");
+    assertEquals("e", e_again.get());
+  }
+
+  //Need Tests for everything from iterables and down
 
   // TODO add more tests here.
 }
